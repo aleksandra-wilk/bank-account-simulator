74d87a4f8df39da36ceac357bb84507f7bb46506
Aleksandra Wilk <wilkaleksandra@outlook.com>
2025-01-11 18:25:23 +0100

    changes in templates

--- main.py
+++ main.py
@@ -1,97 +1,108 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from db.init_db import init_db
 
 
-app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Klucz do sesji
+def create_app():
+
+    app = Flask(__name__)
+
+    # Init bazy danych 
+    init_db()
+
+    # Klucz do sesji
+    app.secret_key = "your_secret_key"
+
+    # Wstępnie zdefiniowany użytkownik
+    LOGIN = "admin"
+    PASSWORD = "admin"
+
+    # Strona logowania
+    @app.route('/', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            # Weryfikacja użytkownika
+            if username == LOGIN and password == PASSWORD:
+                # Logowanie udane - zapisanie użytkownika w sesji
+                session['username'] = username
+                return redirect(url_for('home'))
+            else:
+                # Nieudane logowanie
+                flash("Nieprawidłowa nazwa użytkownika lub hasło.", "danger")
+
+        return render_template('login.html')
 
 
-# Wstępnie zdefiniowany użytkownik
-LOGIN = "admin"
-PASSWORD = "admin"
+    # Strona główna
+    @app.route('/home')
+    def home():
+        if 'username' not in session:
+            return redirect(url_for('login'))  # Użytkownik musi się zalogować
 
 
-# Strona logowania
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        # Weryfikacja użytkownika
-        if username == LOGIN and password == PASSWORD:
-            # Logowanie udane - zapisanie użytkownika w sesji
-            session['username'] = username
-            return redirect(url_for('home'))
-        else:
-            # Nieudane logowanie
-            flash("Nieprawidłowa nazwa użytkownika lub hasło.", "danger")
-
-    return render_template('login.html')
+        return render_template('home_page.html', username=session['username'])
 
 
-# Strona główna
-@app.route('/home')
-def home():
-    if 'username' not in session:
-        return redirect(url_for('login'))  # Użytkownik musi się zalogować
-
-    return render_template('base.html', content_file="home_page.html", username=session['username'])
+    # Produkty - Konta
+    @app.route('/products/accounts')
+    def products_accounts():
+        return render_template('products_accounts.html')
 
 
-
-# Produkty - Konta
-@app.route('/products/accounts')
-def products_accounts():
-    return render_template('base.html', content_file='products_accounts.html')
-
-
-# Produkty - Karty
-@app.route('/products/cards')
-def products_cards():
-    return render_template('base.html', content_file='products_cards.html')
+    # Produkty - Karty
+    @app.route('/products/cards')
+    def products_cards():
+        return render_template('products_cards.html')
 
 
-# Produkty - Pożyczki
-@app.route('/products/loans')
-def products_loans():
-    return render_template('base.html', content_file='products_loans.html')
+    # Produkty - Pożyczki
+    @app.route('/products/loans')
+    def products_loans():
+        return render_template('products_loans.html')
 
 
-# Płatności - Przelew krajowy
-@app.route('/payments/domestic')
-def payments_domestic():
-    return render_template('base.html', content_file='payments_domestic.html')
+    # Płatności - Przelew krajowy
+    @app.route('/payments/domestic')
+    def payments_domestic():
+        return render_template('payments_domestic.html')
 
 
-# Płatności - Przelew własny
-@app.route('/payments/own')
-def payments_own():
-    return render_template('base.html', content_file='payments_own.html')
+    # Płatności - Przelew własny
+    @app.route('/payments/own')
+    def payments_own():
+        return render_template('payments_own.html')
 
 
-# Płatności - Przelew zagraniczny
-@app.route('/payments/foreign')
-def payments_foreign():
-    return render_template('base.html', content_file='payments_foreign.html')
+    # Płatności - Przelew zagraniczny
+    @app.route('/payments/foreign')
+    def payments_foreign():
+        return render_template('payments_foreign.html')
 
 
-# Oferty
-@app.route('/offers')
-def offers():
-    return render_template('base.html', content_file='offers.html')
+    # Oferty
+    @app.route('/offers')
+    def offers():
+        return render_template('offers.html')
 
 
-# Zarządzanie finansami
-@app.route('/financial_management')
-def financial_management():
-    return render_template('base.html', content_file='fin_man.html')
+    # Zarządzanie finansami
+    @app.route('/financial_management')
+    def financial_management():
+        return render_template('fin_man.html')
 
 
-# Wylogowanie
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
+    # Wylogowanie
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for('login'))
+    
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
